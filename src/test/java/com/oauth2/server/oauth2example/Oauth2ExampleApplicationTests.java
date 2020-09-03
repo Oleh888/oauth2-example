8768b102ca08d2b778d0e1be518a099eae488956
Oleh <oleg24453@gmail.com>
2020-09-03 15:11:09 +0300

    first commit

---
+++ src/test/java/com/oauth2/server/oauth2example/Oauth2ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.server.oauth2example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
